ff683d481f7e6584066679e4b264c944830fff0c
FelipeStafoge <felipestafoge11@gmail.com>
2024-02-07 12:58:31 -0300

    Configurando vercel - 3

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 const mongoose = require("mongoose");
-const app = require("./endpoints");
-const connectDB = require("./connectMongo");
+const app = require("./src/endpoints");
+const connectDB = require("./src/connectMongo");
 
 connectDB();
 const PORT = process.env.PORT;
