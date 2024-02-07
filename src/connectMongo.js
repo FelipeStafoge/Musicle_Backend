ef195d8058a479efb2536df6bf6909b08d6835e4
FelipeStafoge <felipestafoge11@gmail.com>
2024-02-07 11:40:41 -0300

    vercel

--- src/connectMongo.js
+++ src/connectMongo.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGODB_CONNECT_URI);
         console.log("Conectado!");
     } catch (error) {
         console.log("Connect failed " + error.message);
