853e81c95b93e4bf20b9958282af6561a35ae30d
FelipeStafoge <felipestafoge11@gmail.com>
2024-02-15 20:21:21 -0300

    bufix: fixes cors

--- index.js
+++ index.js
@@ -6,11 +6,24 @@ connectDB();
 const PORT = process.env.PORT;
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+module.exports = {
+    async headers() {
+      return [
+        {
+        
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    }
+  };
+
+
 
 app.listen(PORT, () => {
     
