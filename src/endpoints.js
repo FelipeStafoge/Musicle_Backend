5263711824c2a68c0afa99bf7d6b917df9ea772e
FelipeStafoge <felipestafoge11@gmail.com>
2024-02-15 00:55:16 -0300

    Alterando horario cronjob para bater com fuso horário

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const cron = require('node-cron');
 const app = express();
 app.use(express.json());
 
