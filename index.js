7f9fe21fed95ed5c5470b57edef518fd251cf38b
FelipeStafoge <felipestafoge11@gmail.com>
2024-02-15 01:55:20 -0300

    Cors adicionado

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const app = require("./src/endpoints");
 const connectDB = require("./src/connectMongo");
-
+const cors = require('cors')
 connectDB();
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.listen(PORT, () => {
     
     
