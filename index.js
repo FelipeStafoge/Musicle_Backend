1e42693ca98a3167f793a0237beaaf22da09372c
FelipeStafoge <felipestafoge11@gmail.com>
2024-02-15 19:31:57 -0300

    Testando liberação CORS

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const app = require("./src/endpoints");
 const connectDB = require("./src/connectMongo");
-const cors = require('cors')
+
 connectDB();
 const PORT = process.env.PORT;
 
-app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 app.listen(PORT, () => {
     
     
