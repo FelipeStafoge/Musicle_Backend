64f8c6fe6faa7dab3bfd3d20156878bc2e447cb0
FelipeStafoge <felipestafoge11@gmail.com>
2024-05-17 01:31:04 -0300

    Added trackName and newImgAlbum on DB.

--- src/endpoints.js
+++ src/endpoints.js
@@ -4,18 +4,21 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
-
 const Artist = mongoose.model('Artist', { 
     artistID: String,
     name: String,
+    trackName: String,
     preview_URL : String,
+    imgAlbu: String,
     tips: Array,    
 });
 
 const Artist_Today = mongoose.model('Artist_Today', { 
     artistID: String,
     name: String,
+    trackName: String,
     preview_URL : String,
+    imgAlbum: String,
     tips: Array,
     todaysDate: String,    
 });
@@ -39,35 +42,43 @@ async function takeArandomArtist(){
     const formattedDay = day < 10 ? '0' + day : day;
     const formattedMonth = month < 10 ? '0' + month : month;
     const formattedDate = `${formattedDay}-${formattedMonth}-${year}`;
+    const link = "https://itunes.apple.com/lookup?id="+random_artist['artistID']+"&entity=song"
+    const response = await fetch(link)
+    const data =  await response.json()
+    let num = getRandomNumber(1,50)
+
+    while (true) {
+        const dataId = await data['results'][num]['artistId']
+        const dataName = await data['results'][num]['artistName']
+        console.log("teste")
+        if (dataId == random_artist['artistID'] && dataName == random_artist['name']){
+            new_preview = data['results'][num]['previewUrl']
+            newTrackName = data['results'][num]['trackName']
+            newImgAlbum = data['results'][num]['artworkUrl100']
+            console.log(newTrackName)
+            console.log(newImgAlbum)
+            break;}
+            else{
+                if(num>=50){
+                    num += 1
+                }else{
+                    num -= 1
+                }
+            }}
+            
     const artist_today = new Artist_Today({
         artistID: random_artist['artistID'],
         name: random_artist['name'],
-        preview_URL: random_artist['preview_URL'],
+        trackName: newTrackName,
+        preview_URL: new_preview,
+        imgAlbum: newImgAlbum,
         tips: random_artist['tips'],
         todaysDate: formattedDate,
-    })
+        })
+
     await artist_today.save();
-    return random_artist;
-}
 
-
-async function changePreviewURL(actual_artist){
-    const link = "https://itunes.apple.com/lookup?id="+actual_artist['artistID']+"&entity=song";
-    const response = await fetch(link);
-    const data =  await response.json();
-    getRandomNumber(1, 50)
-    while (true) {
-        dataId = await data['results'][getRandomNumber(1, 50)]['artistId'];
-        dataName = await data['results'][getRandomNumber(1, 50)]['artistName'];
-        if (dataId == actual_artist['artistID'] && dataName == actual_artist['name']){
-            new_preview = data['results'][getRandomNumber(1, 50)]['previewUrl'];
-            break;
-        }
-    }
-    mongoose_id_artist = actual_artist['_id']
-    const artist = await Artist.findByIdAndUpdate(mongoose_id_artist,{
-        preview_URL: new_preview
-    })
+    return random_artist; 
 }
 
 
@@ -81,6 +92,7 @@ app.post("/post", async (req,res)=>{
     const artist = new Artist({
         artistID: req.body.artistID,
         name: req.body.name,
+        trackName: req.body.trackName,
         preview_URL: req.body.preview_URL,
         tips: req.body.tips
     });
@@ -127,9 +139,8 @@ app.get('/api/allNames', async(req,res)=>{
 let actual_artist;
 
 app.get('/api/cronJobTodayArtist', async (req,res)=>{
-    actual_artist = await takeArandomArtist();
+    actual_artist = await takeArandomArtist(); 
     const deleteone = await Artist_Today.deleteOne();
-    changePreviewURL(actual_artist)
     return res.status(200).send("Novo artista gerado.")
 })
 
